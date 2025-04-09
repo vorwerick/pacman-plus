8da2a0f456dc09cee086580b281ab1bb39c8854d
ales <vorwerick@email.cz>
2025-04-09 12:37:37 +0200

    refac

--- core/src/main/kotlin/cz/pacmanplus/game/core/components/physics/HitPointsComponent.kt
+++ core/src/main/kotlin/cz/pacmanplus/game/core/components/physics/HitPointsComponent.kt
@@ -4,16 +4,17 @@ import com.artemis.Component
 
 //todo refactor
 sealed class HitPoint {
-    data object None : HitPoint()
-    data object Dead : HitPoint()
-    class Alive(var value: Int) : HitPoint()
-    data object Invulnerable : HitPoint()
+    data object None : cz.pacmanplus.game.core.components.physics.HitPoint()
+    data object Dead : cz.pacmanplus.game.core.components.physics.HitPoint()
+    class Alive(var value: Int) : cz.pacmanplus.game.core.components.physics.HitPoint()
+    data object Invulnerable : cz.pacmanplus.game.core.components.physics.HitPoint()
 }
 
 class HitPointsComponent : Component() {
-    var state: HitPoint = HitPoint.None
+    var state: cz.pacmanplus.game.core.components.physics.HitPoint =
+        cz.pacmanplus.game.core.components.physics.HitPoint.None
         get() {
-            if (field == HitPoint.None) {
+            if (field == cz.pacmanplus.game.core.components.physics.HitPoint.None) {
                 throw NotImplementedError()
             }
             return field
